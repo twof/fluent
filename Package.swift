63a761848ef225307777d832ddd8435e5fa06362
Alex Reilly <areilly@yelp.com>
2019-10-03 01:36:24 -0700

    using vapor fork

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.0.0-alpha.2"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha.2"),
+        .package(url: "https://github.com/twof/vapor.git", .branch("platforms")),
     ],
     targets: [
         .target(name: "Fluent", dependencies: ["FluentKit", "Vapor"]),
